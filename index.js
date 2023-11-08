39f867e879a1f7db75c2f7d6ce515f249484216e
YangjoKim <didwh2221@gmail.com>
2023-11-08 16:37:38 +0900

    index.html, index.js 수정

--- index.js
+++ index.js
@@ -16,3 +16,17 @@ function minus(n1, n2) {
         return n2 - n1;
     }
 }
+
+function divide(n1, n2) {
+    if (n2 != 0) {
+        return n1 / n2;
+    }
+}
+
+function log(msg) {
+    console.log(msg);
+}
+
+function log(msg, error) {
+    console.log(msg + ' ' + error);
+}
